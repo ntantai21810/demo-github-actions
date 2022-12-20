0c5e72f0580800a95348abc8327dd575b5578fc4
Nguyen Tai <nguyentai21.fit@gmail.com>
2022-12-20 23:58:54 +0700

    update github action

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 wget https://raw.githubusercontent.com/ntantai21810/demo-github-actions/master/docker-compose.yaml
-docker-compose stop
-docker-compose rm -f
-docker-compose pull
-docker-compose up -d
+docker compose stop
+docker compose rm -f
+docker compose pull
+docker compose up -d
